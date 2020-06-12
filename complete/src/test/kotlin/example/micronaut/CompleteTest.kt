08510076831644fe8a822bcf2e8a155cd6cc226d
Cindy Turpin <TurpinC@ObjectComputing.com>
2020-06-12 08:09:35 -0500

    TESTS FAILING, but committing work done to upgrade to Mn2.0

---
+++ complete/src/test/kotlin/example/micronaut/CompleteTest.kt
@@ -0,0 +1,13 @@
+package example.micronaut
+
+import io.kotlintest.specs.StringSpec
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.annotation.MicronautTest
+
+@MicronautTest
+class CompleteTest(private val application: EmbeddedApplication<*>): StringSpec({
+
+    "test the server is running" {
+        assert(application.isRunning())
+    }
+})
